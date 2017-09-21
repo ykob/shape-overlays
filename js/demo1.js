968425d0725f9e66f5fc1db2b889a34f59338ad4
yoichi kobayashi <info@tplh.net>
2017-09-21 19:04:54 +0900

    [modify] to separate ease object.

--- js/demo1.js
+++ js/demo1.js
@@ -1,11 +1,3 @@
-const ease = {
-  cubicInOut: (t) => {
-    return t < 0.5
-      ? 4.0 * t * t * t
-      : 0.5 * Math.pow(2.0 * t - 2.0, 3.0) + 1.0;
-  },
-}
-
 class GooeyOverlay {
   constructor(elm) {
     this.path1 = elm.querySelector('.js-path-01');

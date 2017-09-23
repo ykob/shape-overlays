9489c02ae1ebe0a263cddaf846ebb72ae4e2dbf4
yoichi kobayashi <info@tplh.net>
2017-09-23 12:53:06 +0900

    [add] basis of toggle overlay.

--- js/demo1.js
+++ js/demo1.js
@@ -1,13 +1,23 @@
 class GooeyOverlay {
   constructor(elm) {
-    this.path1 = elm.querySelector('.js-path-01');
-    this.path2 = elm.querySelector('.js-path-02');
-    this.duration = 500;
-    this.delay = 200;
+    this.elm = elm;
+    this.path = [
+      elm.querySelector('.js-path-01'),
+      elm.querySelector('.js-path-02'),
+      elm.querySelector('.js-path-03'),
+    ];
+    this.numPoints = 18;
+    this.duration = 600;
+    this.delayPointsArray = [];
+    this.delayPoints = 240;
+    this.delayPerPath = 60;
     this.timeStart = Date.now();
     this.isOpened = false;
   }
   toggle() {
+    for (var i = 0; i < this.numPoints + 1; i++) {
+      this.delayPointsArray[i] = Math.random() * this.delayPoints;
+    }
     if (this.isOpened === false) {
       this.open();
     } else {
@@ -16,87 +26,60 @@ class GooeyOverlay {
   }
   open() {
     this.isOpened = true;
+    this.elm.classList.add('is-opened');
     this.timeStart = Date.now();
     this.renderLoop();
   }
   close() {
     this.isOpened = false;
+    this.elm.classList.remove('is-opened');
     this.timeStart = Date.now();
     this.renderLoop();
   }
   updatePathOpen(time) {
-    const p = [
-      ease.cubicInOut(Math.min(Math.max(time - 100, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 200, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 0, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 50, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 200, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 300, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 250, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 200, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 150, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 250, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 100, 0) / this.duration, 1)) * 100,
-    ];
-    return `
-      M  0 0
-      V ${p[0]}
-      C  5 ${p[0]}  5 ${p[1]} 10 ${p[1]}
-      C 15 ${p[1]} 15 ${p[2]} 20 ${p[2]}
-      C 25 ${p[2]} 25 ${p[3]} 30 ${p[3]}
-      C 35 ${p[3]} 35 ${p[4]} 40 ${p[4]}
-      C 45 ${p[4]} 45 ${p[5]} 50 ${p[5]}
-      C 55 ${p[5]} 55 ${p[6]} 60 ${p[6]}
-      C 65 ${p[6]} 65 ${p[7]} 70 ${p[7]}
-      C 75 ${p[7]} 75 ${p[8]} 80 ${p[8]}
-      C 85 ${p[8]} 85 ${p[9]} 90 ${p[9]}
-      C 95 ${p[9]} 95 ${p[10]} 100 ${p[10]}
-      V 0
-      H 0
-    `;
+    const points = [];
+    for (var i = 0; i < this.numPoints + 1; i++) {
+      points[i] = ease.quadraticInOut(Math.min(Math.max(time - this.delayPointsArray[i], 0) / this.duration, 1)) * 100
+    }
+
+    let str = `M 0 0 V ${points[0]} `;
+    for (var i = 0; i < this.numPoints; i++) {
+      const p = (i + 1) / this.numPoints * 100;
+      const cp = p - (1 / this.numPoints * 100) / 2;
+      str += `C ${cp} ${points[i]} ${cp} ${points[i + 1]} ${p} ${points[i + 1]} `;
+    }
+    str += `V 0 H 0`;
+    return str;
   }
   updatePathClose(time) {
-    const p = [
-      ease.cubicInOut(Math.min(Math.max(time - 100, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 200, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 0, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 50, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 200, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 300, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 250, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 200, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 150, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 250, 0) / this.duration, 1)) * 100,
-      ease.cubicInOut(Math.min(Math.max(time - 100, 0) / this.duration, 1)) * 100,
-    ];
-    return `
-      M  0 ${p[0]}
-      C  5 ${p[0]}  5 ${p[1]} 10 ${p[1]}
-      C 15 ${p[1]} 15 ${p[2]} 20 ${p[2]}
-      C 25 ${p[2]} 25 ${p[3]} 30 ${p[3]}
-      C 35 ${p[3]} 35 ${p[4]} 40 ${p[4]}
-      C 45 ${p[4]} 45 ${p[5]} 50 ${p[5]}
-      C 55 ${p[5]} 55 ${p[6]} 60 ${p[6]}
-      C 65 ${p[6]} 65 ${p[7]} 70 ${p[7]}
-      C 75 ${p[7]} 75 ${p[8]} 80 ${p[8]}
-      C 85 ${p[8]} 85 ${p[9]} 90 ${p[9]}
-      C 95 ${p[9]} 95 ${p[10]} 100 ${p[10]}
-      V 100
-      H 0
-    `;
+    const points = [];
+    for (var i = 0; i < this.numPoints + 1; i++) {
+      points[i] = ease.quadraticInOut(Math.min(Math.max(time - this.delayPointsArray[i], 0) / this.duration, 1)) * 100
+    }
+
+    let str = `M 0 ${points[0]} `;
+    for (var i = 0; i < this.numPoints; i++) {
+      const p = (i + 1) / this.numPoints * 100;
+      const cp = p - (1 / this.numPoints * 100) / 2;
+      str += `C ${cp} ${points[i]} ${cp} ${points[i + 1]} ${p} ${points[i + 1]} `;
+    }
+    str += `V 100 H 0`;
+    return str;
   }
   render() {
     if (this.isOpened) {
-      this.path1.setAttribute('d', this.updatePathOpen(Date.now() - this.timeStart));
-      this.path2.setAttribute('d', this.updatePathOpen(Date.now() - (this.timeStart + this.delay)));
+      for (var i = 0; i < this.path.length; i++) {
+        this.path[i].setAttribute('d', this.updatePathOpen(Date.now() - (this.timeStart + this.delayPerPath * i)));
+      }
     } else {
-      this.path1.setAttribute('d', this.updatePathClose(Date.now() - (this.timeStart + this.delay)));
-      this.path2.setAttribute('d', this.updatePathClose(Date.now() - this.timeStart));
+      for (var i = 0; i < this.path.length; i++) {
+        this.path[i].setAttribute('d', this.updatePathClose(Date.now() - (this.timeStart + this.delayPerPath * (this.path.length - i - 1))));
+      }
     }
   }
   renderLoop() {
     this.render();
-    if (Date.now() - this.timeStart < this.duration + this.delay + 300) {
+    if (Date.now() - this.timeStart < this.duration + this.delayPerPath * (this.path.length - 1) + this.delayPoints) {
       requestAnimationFrame(() => {
         this.renderLoop();
       });

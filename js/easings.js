d2f6e8abefe9c25fe75405b59e41cc27b901e221
yoichi kobayashi <info@tplh.net>
2017-09-23 12:50:33 +0900

    increase easing functions.

--- js/easings.js
+++ js/easings.js
@@ -1,7 +1,21 @@
 const ease = {
+  circularInOut: (t) => {
+    return t < 0.5
+        ? 0.5 * (1.0 - Math.sqrt(1.0 - 4.0 * t * t))
+        : 0.5 * (Math.sqrt((3.0 - 2.0 * t) * (2.0 * t - 1.0)) + 1.0);
+  },
   cubicInOut: (t) => {
     return t < 0.5
       ? 4.0 * t * t * t
       : 0.5 * Math.pow(2.0 * t - 2.0, 3.0) + 1.0;
+  },
+  quadraticInOut: (t) => {
+    const p = 2.0 * t * t;
+    return t < 0.5 ? p : -p + (4.0 * t) - 1.0;
+  },
+  quarticInOut: (t) => {
+    return t < 0.5
+        ? +8.0 * Math.pow(t, 4.0)
+        : -8.0 * Math.pow(t - 1.0, 4.0) + 1.0;
   },
 }

31de00e798b734855b1cfff28d256a9cd514f7f7
yoichi kobayashi <info@tplh.net>
2017-09-25 16:47:09 +0900

    [update] remvoed unused easing functions.

--- js/easings.js
+++ js/easings.js
@@ -18,16 +18,4 @@ const ease = {
       ? 4.0 * t * t * t
       : 0.5 * Math.pow(2.0 * t - 2.0, 3.0) + 1.0;
   },
-  quadraticInOut: (t) => {
-    const p = 2.0 * t * t;
-    return t < 0.5 ? p : -p + (4.0 * t) - 1.0;
-  },
-  quarticIn: (t) => {
-    return t * t;
-  },
-  quarticInOut: (t) => {
-    return t < 0.5
-        ? +8.0 * Math.pow(t, 4.0)
-        : -8.0 * Math.pow(t - 1.0, 4.0) + 1.0;
-  },
 }
